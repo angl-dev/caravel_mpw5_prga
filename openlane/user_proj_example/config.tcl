2c8f2d8c4f3a84cfaa102c239a37fae996fa6cbd
Ang Li <angl@princeton.edu>
2022-02-25 11:03:49 -0500

    update tile_clb pin order

--- openlane/user_proj_example/config.tcl
+++ openlane/user_proj_example/config.tcl
@@ -36,7 +36,7 @@ set ::env(CLOCK_PERIOD) "100"
 set ::env(FP_SIZING) absolute
 set ::env(DIE_AREA) "0 0 250 250"
 
-#set ::env(FP_PIN_ORDER_CFG) $script_dir/pin_order.cfg
+set ::env(FP_PIN_ORDER_CFG) $script_dir/pin_order.cfg
 
 set ::env(PL_BASIC_PLACEMENT) 1
 set ::env(PL_TARGET_DENSITY) 0.3
