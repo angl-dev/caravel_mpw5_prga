895b46b28e115cbd465e4762f134ae078d4c8187
Ang Li <angl@princeton.edu>
2022-03-08 19:36:21 -0500

    [RTL] 8x8x8 PRGA architecture

--- verilog/rtl/tile_clb.bb.v
+++ verilog/rtl/tile_clb.bb.v
@@ -3,10 +3,10 @@ module tile_clb (
     inout vccd1,	// User area 1 1.8V supply
     inout vssd1,	// User area 1 digital ground
 `endif
-    output wire [19:0] cu_x0y0n_L1
-    , output wire [19:0] cu_x0y0s_L1
-    , input wire [19:0] bi_u1y0n_L1
-    , input wire [19:0] bi_u1y0s_L1
+    output wire [11:0] cu_x0y0n_L1
+    , output wire [11:0] cu_x0y0s_L1
+    , input wire [11:0] bi_u1y0n_L1
+    , input wire [11:0] bi_u1y0s_L1
     , input wire [0:0] clk
     , input wire [0:0] prog_clk
     , input wire [0:0] prog_rst
