6f0b9a943dcbaadd68a0981aa3fe57f76a6f2725
getziadz <getziadz@pm.me>
2022-03-15 13:57:21 -0400

    RTL, GDS, and LEF files for almost complete design  - RTL for integration with wrapper  - user_project_wrapper GDS and LEF files

--- verilog/rtl/tile_clb.bb.v
+++ verilog/rtl/tile_clb.bb.v
@@ -1,20 +1,3 @@
-module tile_clb (
-`ifdef USE_POWER_PINS
-    inout vccd1,	// User area 1 1.8V supply
-    inout vssd1,	// User area 1 digital ground
-`endif
-    output wire [11:0] cu_x0y0n_L1
-    , output wire [11:0] cu_x0y0s_L1
-    , input wire [11:0] bi_u1y0n_L1
-    , input wire [11:0] bi_u1y0s_L1
-    , input wire [0:0] clk
-    , input wire [0:0] prog_clk
-    , input wire [0:0] prog_rst
-    , input wire [0:0] prog_done
-    , input wire [0:0] prog_we
-    , input wire [0:0] prog_din
-    , output wire [0:0] prog_dout
-    , output wire [0:0] prog_we_o
-    );
-
-endmodule
+version https://git-lfs.github.com/spec/v1
+oid sha256:91f0ee18b4aaea4d980dcc5e7a6d1ac7646195c87fd9821ce87b07bee67fd1fe
+size 555
